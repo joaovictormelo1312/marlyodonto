83d37a7ba2798493f7b2d51820d3f0e5e35b0263
joaovictormelo1312 <joaovictormelo2005@gmail.com>
2026-08-25 11:17:58 -0300

    “ajustes”

--- src/lib/site-config.ts
+++ src/lib/site-config.ts
@@ -11,8 +11,8 @@ export const siteConfig = {
     message: "Olá! Gostaria de agendar uma consulta com a Dra. Teresa Marly.",
   },
   address: {
-    full: "Ed. Manhattan River Center - Avenida Senador Arêa Leão 2185, R. Prof. Elías Tôrres, 1 - Sl 1211, Teresina - PI, 64051-090",
-    short: "Manhattan River Center, Teresina - PI",
+    full: "Ed. Manhattan River Center - Avenida Senador Arêa Leão 2185, R. Prof. Elías Tôrres, 1 - Sala: 1211, Teresina - PI, 64051-090",
+    short: "Manhattan River Center, Teresina - PI ",
     mapsUrl:
       "https://www.google.com/maps/search/?api=1&query=Ed.+Manhattan+River+Center,+Avenida+Senador+Arêa+Leão+2185,+Teresina+-+PI,+64051-090",
   },
